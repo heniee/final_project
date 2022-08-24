edc3cc875c9f0f65673afc6769006a23eb0a0b17
heniee <jhh93601@naver.com>
2022-08-24 15:38:53 +0900

    예외처리

--- 220822_mproject/src/main/java/com/example/demo/advice/BoardControllerAdvice.java
+++ 220822_mproject/src/main/java/com/example/demo/advice/BoardControllerAdvice.java
@@ -1,14 +1,36 @@
 package com.example.demo.advice;
 
+import java.sql.SQLException;
+
 import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.dto.*;
+import com.example.demo.exception.BoardNotFoundException;
+import com.example.demo.exception.JobFailException;
 
 @RestControllerAdvice
 public class BoardControllerAdvice {
+	
+	@ExceptionHandler(SQLException.class)
+	public ResponseEntity<String> sQLException(SQLException e){
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+	}
+	
+	@ExceptionHandler(BoardNotFoundException.class)
+	public ResponseEntity<String> BNFExption(){
+		return ResponseEntity.status(HttpStatus.CONFLICT).body("게시물을 찾을 수 없습니다");
+		
+	}
+	
+	@ExceptionHandler(JobFailException.class)
+	public ResponseEntity<String> JFExption(JobFailException e){
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+		
+	}
+	
     @ExceptionHandler(ConstraintViolationException.class)
 	public ResponseEntity<String> constraintViolatException() {
 		   return ResponseEntity.status(HttpStatus.CONFLICT).body("데이터가 누락되었습니다");
--- 220822_mproject/src/main/java/com/example/demo/service/BoardService.java
+++ 220822_mproject/src/main/java/com/example/demo/service/BoardService.java
@@ -12,9 +12,20 @@ public class BoardService {
 	@Autowired
 	private BoardDao boardDao;
 
+	
+	// 글쓰기 : 실패하면 409
 	public Board write(BoardDto.Write dto, String loginId) {
 		Board board = dto.toEntity().addWriter(loginId);
 		boardDao.save(board);
 		return board;
 	}
+	
+	// 글읽기 : 글이 없으면 409. 글이 있고 글쓴이면 조회수 증가 
+	
+	// 글목록 : 글이 없으면 빈 목록 
+	
+	// 글변경 : 실패 - 글이 없다(BoardNotFouneException), 글쓴이가 아니다(JobFailException)
+	
+	// 글삭제 : 실패 - 글이 없다(BNFE), 글쓴이가 아니다(JFE)
+	
 }
